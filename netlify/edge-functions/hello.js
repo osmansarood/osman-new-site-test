b666192e1fe863e0b096378851db9fb6e26670aa
Osman Sarood <osmansarood@gmail.com>
2022-09-09 16:30:53 -0700

    Revert "adding edge func logs"
    This reverts commit f38a8c05f0f82ac0900ee597e4f2a4cc2ad7b9a2.

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -3,3 +3,5 @@ import type { Context } from "https://edge.netlify.com";
 export default async (request: Request, context: Context) => {
   context.log("Hello from the logging service");
 };
+
+export default () => new Response("Hello world");
