6b5f39326e781c4006d3ebca5cdbf620b1aa123a
Osman Sarood <osmansarood@gmail.com>
2022-09-20 13:49:19 -0700

    test

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,2 +1,2 @@
-console.log("Hello world! .. this is a good log line again...")
+console.log("Hello world! .. this is a good log line again... 20sept")
 export default () => new Response("Hello world");
